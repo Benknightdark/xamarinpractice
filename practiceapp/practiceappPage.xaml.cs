db9113a5cb56f5c70d87a9000074f0cf03b11dcd
Ben <moon791017@gmail.com>
2017-07-26 13:42:36 +0800

    OK

---
+++ practiceapp/practiceappPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace practiceapp
+{
+    public partial class practiceappPage : ContentPage
+    {
+        private   info Info;
+        public practiceappPage()
+        {
+            InitializeComponent();
+            Info = new info();
+            entry.BindingContext = Info;
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Info.Name = "fdsfasdfasd1111" + entry.Text.ToString();
+           // DisplayAlert(string.Empty, Info.Name.ToString(),"ok");
+   
+        }
+
+       
+    }
+}
